2f790ab4170c11569ee2ef320e6723ea1ffdd957
wigor <wigor21@hotmail.com>
2024-03-27 23:25:41 -0300

    feat: created header components

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+import { FiUser, FiLogOut } from "react-icons/fi";
+
+export const Header = () => {
+  return (
+    <header className="w-full flex items-center px-2 py-4 bg-white h-20 shadow-sm">
+      <div className="w-full flex items-center justify-between max-w-7xl mx-auto">
+        <Link href="/">
+          <h1 className="font-bolt text-2xl pl-1 hover:tracking-wider duration-300">
+            <span className="text-blue-500">DEV</span> CONTROLE
+          </h1>
+        </Link>
+
+        <div className="flex items-center gap-4">
+          <Link href="/dashboard">
+            <FiUser size={26} color="#4b5563" />
+          </Link>
+
+          <button>
+            <FiLogOut size={26} color="#4b5563" />
+          </button>
+        </div>
+      </div>
+    </header>
+  );
+};
