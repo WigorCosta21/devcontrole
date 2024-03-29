8475f9f4d71282609f03f219d638ee38df48eaf0
wigor <wigor21@hotmail.com>
2024-03-29 18:39:51 -0300

    created call table

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,7 +1,9 @@
+import Link from "next/link";
 import { getServerSession } from "next-auth";
 import { redirect } from "next/navigation";
 import { authOptions } from "@/lib/auth";
 import { Container } from "@/components/Container";
+import { TicketItem } from "./components/Ticket";
 
 const Dashboard = async () => {
   const session = await getServerSession(authOptions);
@@ -12,7 +14,34 @@ const Dashboard = async () => {
 
   return (
     <Container>
-      <h1>Página Dashboard</h1>
+      <main className="mt-9 mb-2">
+        <div className="flex items-center justify-between">
+          <h1 className="text-3xl font-bold">Chamados</h1>
+          <Link
+            href="/dashboard/new"
+            className="bg-blue-500 px-4 py-2 rounded text-white"
+          >
+            Abrir chamado
+          </Link>
+        </div>
+
+        <table className="min-w-full my-2">
+          <thead>
+            <tr>
+              <th className="font-medium text-left pl-1">CLIENTE</th>
+              <th className="font-medium text-left hidden sm:block">
+                DATA CADASTRO
+              </th>
+              <th className="font-medium text-left">STATUS</th>
+              <th className="font-medium text-left">#</th>
+            </tr>
+          </thead>
+          <tbody>
+            <TicketItem />
+            <TicketItem />
+          </tbody>
+        </table>
+      </main>
     </Container>
   );
 };
