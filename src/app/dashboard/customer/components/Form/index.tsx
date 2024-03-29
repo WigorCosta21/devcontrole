305a39b7c061b4149567d7509337e1532ca2da52
wigor <wigor21@hotmail.com>
2024-03-29 19:13:21 -0300

    feat: configuring zod

---
+++ src/app/dashboard/customer/components/Form/index.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schema = z.object({
+  name: z.string().min(1, "O campo nome é obrigatório"),
+  email: z
+    .string()
+    .email("Digite um email válido")
+    .min(1, "O email é obrigatório"),
+  phone: z.string().refine(
+    (value) => {
+      return (
+        /^(?:\(\d{2}\)\s?)?d(9)$/.test(value) ||
+        /^d{2}\s\d{9}$/.test(value) ||
+        /^\d{11}$/.test(value)
+      );
+    },
+    {
+      message: "O número telefone deve estar (DD) 999999999",
+    }
+  ),
+  address: z.string().optional(),
+});
+
+type FormData = z.infer<typeof schema>;
+
+export const NewCustomerForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormData>({
+    resolver: zodResolver(schema),
+  });
+
+  return (
+    <form>
+      <label htmlFor="">Nome completo</label>
+      <input type="text" placeholder="Digite o nome completo" />
+    </form>
+  );
+};
--- src/app/dashboard/customer/new/page.tsx
+++ src/app/dashboard/customer/new/page.tsx
@@ -1,9 +1,30 @@
+import Link from "next/link";
+import { getServerSession } from "next-auth";
+import { redirect } from "next/navigation";
+import { authOptions } from "@/lib/auth";
 import { Container } from "@/components/Container";
+import { NewCustomerForm } from "../components/Form";
 
-const NewCustomer = () => {
+const NewCustomer = async () => {
+  const session = await getServerSession(authOptions);
+
+  if (!session || !session.user) {
+    redirect("/");
+  }
   return (
     <Container>
-      <h1>Página Cadastro Cliente</h1>
+      <main className="flex flex-col mt-9 mb-2">
+        <div className="flex items-center gap-3">
+          <Link
+            href="/dashboard/customer"
+            className="bg-gray-900 px-4 py-1 text-white rounded"
+          >
+            Voltar
+          </Link>
+          <h1 className="text-3xl font-bold">Novo cliente</h1>
+        </div>
+        <NewCustomerForm />
+      </main>
     </Container>
   );
 };
