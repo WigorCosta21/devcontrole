315f3893d378a23a79367f39573ff1224e5fac43
wigor <wigor21@hotmail.com>
2024-03-28 23:46:45 -0300

    feat: created home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,22 @@
+import Image from "next/image";
+
+import heroImg from "@/assets/hero.svg";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Dev Controle</h1>
-    </div>
+    <main className="flex items-center flex-col min-h-[calc(100vh-80px)]">
+      <h2 className="font-medium text-2xl mb-2">Gerencie sua empresa</h2>
+      <h1 className="font-bold text-3xl mb-8 text-blue-600 md:text-4xl">
+        Atendimento, clientes
+      </h1>
+
+      <Image
+        src={heroImg}
+        alt="Imagem hero do dev controle"
+        width={600}
+        className="max-w-sm md:max-w-xl"
+      />
+    </main>
   );
 };
 
